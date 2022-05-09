5f5f1c778dbd5b8bcf100a841d67856d95864a04
juncongH <1300388361@qq.com>
2022-05-10 00:35:05 +0800

    JavaConfig配置Bean装配

---
+++ Spring/src/main/java/Spring04/SpringConfig.java
@@ -0,0 +1,17 @@
+package Spring04;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description: 基于Java的Bean装配
+ * @Author hujuncong
+ * @Title: SpringConfig
+ * @Package Spring04
+ * @Date 2022/5/10 00:28
+ */
+@Configuration
+@ComponentScan(basePackages = "Spring03")
+public class SpringConfig {
+
+}
--- Spring/src/test/java/String03/StudentTest.java
+++ Spring/src/test/java/String03/StudentTest.java
@@ -2,12 +2,15 @@ package String03;
 
 import Spring03.Student;
 import Spring03.service.StudentServiceImpl;
+import Spring04.SpringConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class StudentTest {
     public static void main(String[] args) {
-        ApplicationContext context=new ClassPathXmlApplicationContext("bean03.xml");
+//        ApplicationContext context=new ClassPathXmlApplicationContext("bean03.xml");
+        ApplicationContext context=new AnnotationConfigApplicationContext(SpringConfig.class);
         StudentServiceImpl studentServiceImpl= (StudentServiceImpl) context.getBean("studentServiceImpl");
         System.out.println(studentServiceImpl.run());
     }
